8313c30cac8da413503d609f3d151109c35eafe5
mauritsderoover <50581714+mauritsderoover@users.noreply.github.com>
2020-04-24 20:02:13 +0200

    small update

--- application/text_checker/text_checker.py
+++ application/text_checker/text_checker.py
@@ -6,28 +6,19 @@ text_bp = Blueprint("text_bp", __name__, template_folder="templates")
 
 @text_bp.route("/search1", methods=["GET", "POST"])
 def check_text():
-    input = request.form.get("input")
-    print(input)
-    if input:
-        text, score = checker(input)
-        print(text, score)
+    if not request.form.get("user_input"):
+        if not request.args.get("user_input"):
+            return "input was false"
+        else:
+            text, score = checker(request.args.get("user_input"))
+            return text + " with a score of " + str(score)
+    else:
+        text, score = checker(request.form.get("user_input"))
         return text + " with a score of " + str(score)
-    return "didnt work"
 
 
 @text_bp.route("/search2/<user_input>", methods=["GET", "POST"])
 def check_text2(user_input):
-    print(user_input)
-    if user_input:
-        text, score = checker(user_input)
-        print(text, score)
-        return text + " with a score of " + str(score)
-    return "didnt work"
-
-
-@text_bp.route("/search3", methods=["GET", "POST"])
-def check_text3():
-    user_input = request.args.get("input")
     print(user_input)
     if user_input:
         text, score = checker(user_input)
