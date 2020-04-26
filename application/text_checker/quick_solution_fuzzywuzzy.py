f23ccfb9fefd473c6a5ac66a5e8390b1c76f1020
mauritsderoover <50581714+mauritsderoover@users.noreply.github.com>
2020-04-26 03:07:40 +0200

    front end changes

--- application/text_checker/quick_solution_fuzzywuzzy.py
+++ application/text_checker/quick_solution_fuzzywuzzy.py
@@ -2,6 +2,7 @@ import pandas as pd
 from fuzzywuzzy import fuzz, process
 import json
 
+
 # %%
 
 
@@ -39,10 +40,38 @@ def checker(input_text, path="rel"):
         "label": relevant_row["label"].iloc[0],
         "title": relevant_row["title"].iloc[0],
         "explanation": relevant_row["explanation"].iloc[0],
-        "score":  score
+        "score": score
     }
 
     return json_format
+
+
+def checker_options(input_text, path="rel"):
+    absolute_path = "C:/personal projects/fakenews/data/data_poynter_COMPLETE_2020-04-24.csv"
+    relative_path = "data/data_poynter_COMPLETE_2020-04-24.csv"
+
+    if path == "rel":
+        df = pd.read_csv(relative_path)
+    elif path == "abs":
+        df = pd.read_csv(absolute_path)
+
+    our_titles = df["title"].values
+
+    output = process.extract(input_text, our_titles, scorer=fuzz.token_set_ratio, limit=3)
+
+    output_list = []
+    for item in output:
+        relevant_row = df[df["title"] == item[0]]
+        output_list.append({
+            "fact_checker": relevant_row["fact_checker"].iloc[0],
+            "date": relevant_row["date"].iloc[0],
+            "location": relevant_row["location"].iloc[0],
+            "label": relevant_row["label"].iloc[0],
+            "title": relevant_row["title"].iloc[0],
+            "explanation": relevant_row["explanation"].iloc[0],
+            "score": item[1]
+        })
+    return output_list
 
 
 def checker_test(input_text, path="rel"):
@@ -65,4 +94,7 @@ if __name__ == "__main__":
     # print(basic_checker("India has not outlawed social media", "abs"))
     print(checker("5G causes flu", "abs"))
     df, result = checker_test("5G causes flu", "abs")
-    test_result = fuzz.token_set_ratio("5G causes flu", "The 5G network causes flu symptoms and it caused the covid-19 pandemic")
+    test_result = fuzz.token_set_ratio("5G causes flu",
+                                       "The 5G network causes flu symptoms and it caused the covid-19 pandemic")
+    testing = checker_options("5G causes flu", "abs")
+    print(checker_options("5G causes flu", "abs"))
--- application/text_checker/text_checker.py
+++ application/text_checker/text_checker.py
@@ -1,13 +1,12 @@
 from flask import Blueprint, request, jsonify
-from application.text_checker.quick_solution_fuzzywuzzy import basic_checker, checker
+from application.text_checker.quick_solution_fuzzywuzzy import basic_checker, checker, checker_options
+import json
 
 text_bp = Blueprint("text_bp", __name__, template_folder="templates")
 
 
 @text_bp.route("/search1", methods=["GET", "POST"])
 def check_text():
-    print(request.is_json())
-    print("is this executed?")
     if not request.form.get("user_input"):
         if not request.args.get("user_input"):
             return "input was false"
@@ -16,17 +15,15 @@ def check_text():
             if isinstance(input, list):
                 output = []
                 for item in input:
-                    output.append(checker(item))
+                    output.append(checker_options(item))
                 return jsonify({"output": output})
             elif isinstance(input, str):
-                print("we got to the str part")
-                output = checker(input)
-                print({"output": output})
+                output = checker_options(input)
                 return jsonify({"output": output})
             else:
                 return jsonify({"output": None})
     else:
-        response = checker(request.form.get("user_input"))
+        response = checker_options(request.form.get("user_input"))
         return response
 
 
